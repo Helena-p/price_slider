3a83bd89948320bc4b72ac02f4050bc7ebddf827
Helena <helenasleterkesto@gmail.com>
2021-12-30 15:39:46 +0100

    Add array of price options to refaktor conditional price calculations later

---
+++ src/script/priceOptions.js
@@ -0,0 +1,24 @@
+const options = [
+	{
+		low: 0,
+		high: 50,
+		price: 8,
+	},
+	{
+		low: 50,
+		high: 100,
+		price: 12,
+	},
+	{
+		low: 100,
+		high: 500,
+		price: 16,
+	},
+	{
+		low: 500,
+		high: 1000,
+		price: 24,
+	},
+];
+
+export default options;
